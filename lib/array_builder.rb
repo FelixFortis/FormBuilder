53dca117c7459bc51ccd5c62cd62c08e85348c5e
Adam Wenham <adamwenham64@gmail.com>
2014-08-10 13:03:19 +0100

    finished rspec config, got red spec

--- lib/array_builder.rb
+++ lib/array_builder.rb
@@ -1,2 +1,5 @@
 class ArrayBuilder
+  def initialize
+
+  end
 end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -2,6 +2,9 @@ require 'capybara/rspec'
 require 'rubygems'
 require 'factory_girl'
 
+require_relative '../lib/array_builder'
+require_relative '../lib/xml_builder'
+
 RSpec.configure do |config|
   config.include FactoryGirl::Syntax::Methods
 end
