b721588439a345ebfebaeac3e1d073d6f83c7d38
FelixFortis <adamwenham64@gmail.com>
2014-08-12 07:09:21 +0000

    green specs and cukes for ArrayBuilder

--- lib/array_builder.rb
+++ lib/array_builder.rb
@@ -1,25 +1,43 @@
 require_relative '../requirements_handler'
 
 class ArrayBuilder
-  attr_reader :range_to_build
+	attr_reader :range_to_build, :first_percent_field, :section_fields
   def initialize(args={})
     @range_to_build = args[:range_to_build]
-    @first_percent_field = args[:first_percent_field]
+	@first_percent_field = args[:first_percent_field]
+	@section_fields = args[:section_fields]
   end
-    
+
   def build_array
     @array_to_build = @range_to_build.to_a
   end
 
-  def format_array
-      unless build_array.nil?
-      @final_array = build_array.collect do |field|
+  def format_array(array)
+    unless array.nil?
+      @final_array = array.collect do |field|
         field.to_s.rjust(3,"0").prepend("field_")
       end
     end
   end
     
-  def get_percent_fields
+  def get_all_fields_array
+	format_array(build_array)
+  end
+    
+  def get_percent_fields_array
+	count = @first_percent_field
+	stop = build_array.size
+	basic_percent_array = []
+	
+	while count <= stop do
+	  basic_percent_array << count
+      count += 3
+	end
+	
+	format_array(basic_percent_array)
+  end
+    
+  def get_section_fields_array
     
   end
 end
--- spec/features/array_builder_spec.rb
+++ spec/features/array_builder_spec.rb
@@ -25,7 +25,7 @@ describe ArrayBuilder do
     it "should convert the range into the all_fields array" do
       range_to_build = (1..10)
       range_handler = ArrayBuilder.new(range_to_build: range_to_build)
-        converted = range_handler.format_array
+        converted = range_handler.get_all_fields_array
 
       expect(converted).to eq(["field_001", "field_002", "field_003", "field_004", "field_005", "field_006", "field_007", "field_008", "field_009", "field_010"])
     end
@@ -33,9 +33,17 @@ describe ArrayBuilder do
     it "should convert the range into the percent_fields array" do
       range_to_build = (1..11)
       range_handler = ArrayBuilder.new(range_to_build: range_to_build, first_percent_field: 2)
-      converted = range_handler.build_percent_fields_array
+	  converted = range_handler.get_percent_fields_array
 
       expect(converted).to eq(["field_002", "field_005", "field_008", "field_011"])
     end
+	  
+	it "should convert the array into the section fields array" do
+	  array_to_build = [1,10,100]
+	  array_handler = ArrayBuilder.new(section_fields: array_to_build)
+	  converted = array_handler.format_array(array_to_build)
+	  
+	  expect(converted).to eq(["field_001", "field_010", "field_100"])
+	end
   end
 end
