e78a3f826cefb92e591028a15b613483d59f3e55
FelixFortis <adamwenham64@gmail.com>
2014-08-23 15:20:57 +0000

    added new spec and fixed nomethod errors

--- lib/xml_builder.rb
+++ lib/xml_builder.rb
@@ -6,6 +6,18 @@ class XMLBuilder
 	end
 	
 	def build_xml
-		%{<SmartFormConfig><Sections><Section><Title count="0">field_0</Title><FriendName>My Form</FriendName><Order>0</Order><HeaderHTML>My Form</HeaderHTML><Label>Welcome to my form...</Label></Section></Sections><ContactFields></ContactFields><Segmentation></Segmentation><BespokeFields></BespokeFields><ContactFieldFlags></ContactFieldFlags></SmartFormConfig>}
+		%{<SmartFormConfig><Sections><Section><Title count="0">field_0</Title><FriendName>My Form</FriendName><Order>0</Order><HeaderHTML>My Form</HeaderHTML><Label>Welcome to my form...</Label></Section>#{sections_xml}</Sections><ContactFields>#{contact_fields_xml}</ContactFields><Segmentation>#{segmentation_xml}</Segmentation><BespokeFields>#{bespoke_fields_xml}</BespokeFields><ContactFieldFlags></ContactFieldFlags></SmartFormConfig>}
+	end
+	
+	def sections_xml
+	end
+	
+	def contact_fields_xml
+	end
+	
+	def segmentation_xml
+	end
+	
+	def bespoke_fields_xml
 	end
 end
